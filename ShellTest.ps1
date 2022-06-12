42e142f48ae3463261b7f45e9e6748c5c87e99d1
Yoshii64 <100977835+Yoshii64@users.noreply.github.com>
2022-06-12 18:42:10 -0400

    more stuff

--- ShellTest.ps1
+++ ShellTest.ps1
@@ -4,6 +4,7 @@ $FormObject = [System.Windows.Forms.Form]
 $LabelObject = [System.Windows.Forms.Label]
 $ButtonObject = [System.Windows.Forms.Button]
 
+
 $Window=New-Object $FormObject
 $Window.ClientSize ='650,650'
 $Window.BackColor = '#ffffff'
@@ -23,8 +24,20 @@ $network.CLientSize = '100,100'
 $network.location = New-Object System.Drawing.Point(280,100)
 $network.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
+$fix = New-Object $ButtonObject
+$fix.text = 'fix errors in Windows'
+$fix.ClientSize = '100,100'
+$fix.location = New-Object System.Drawing.Point(280,200)
+$fix.Font = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$Window.Controls.AddRange(@($CleanupSystem, $network))
+$OneDriveUninstall = New-Object $ButtonObject
+$OneDriveUninstall.text = 'uninstall OneDrive'
+$OneDriveUninstall.ClientSize = '100,100'
+$OneDriveUninstall.Location = '180,0'
+$OneDriveUninstall.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 12)
+
+
+$Window.Controls.AddRange(@($CleanupSystem, $network, $fix, $OneDriveUninstall))
 
 
 $CleanupSystem.Add_Click({
@@ -37,7 +50,7 @@ Get-ChildItem -Path "C:\Windows\SoftwareDistribution\Download" *.* -Recurse | Re
 Get-ChildItem -Path "C:\Windows\Prefetch" *.* -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
 })
 
-$network.Add_click({
+$count = $network.Add_click({
 Clear-Host
 Write-Output clearing network cache...
 IPCONFIG /release
@@ -49,7 +62,33 @@ echo setting optimizations for network...
 netsh int tcp set supplemental
 netsh int tcp set heuristics disabled
 netsh int tcp set global timestamps=disabled
+Write-Output setting up DNS optimizations...
+netsh interface ip delete dnsservers "Local Area Connection" all
+netsh interface ip add dns name="Local Area Connection" addr=8.8.4.4 index=1
+netsh interface ip add dns name="Local Area Connection" addr=8.8.8.8 index=2
+ipconfig /all | findstr /c:"8.8.4.4"
+ipconfig /all | findstr /c:"8.8.8.8"
+Write-Output done
 })
+
+
+$fix.Add_Click({
+sfc /scannow
+DISM /online /cleanup-image /RestoreHealth
+sfc /scannow
+CHKDSK /F
+})
+ 
+ $OneDriveUninstall.Add_Click({
+Write-Output uninstalling onedrive...
+Write-Output killing OneDrive processes...
+taskkill /f /im "OneDrive.exe" -ErrorAction SilentlyContinue
+taskkill /F /IM "explorer.exe"
+Remove-Item -Force C:\Windows\System32\OneDrive.exe -ErrorAction SilentlyContinue
+Remove-Item -Force $env:UserProfile\AppData\Local\Microsoft\OneDrive -ErrorAction SilentlyContinue
+start Explorer.exe
+ })
+
 
 
 $Window.ShowDialog()
